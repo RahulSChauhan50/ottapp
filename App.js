a7dcb2f7d6eebbe286370d5c340e2e921ae6598e
RahulSChauhan50 <rahulschauhan50@gmail.com>
2022-01-19 23:38:31 +0530

    Added React Navigation

--- App.js
+++ App.js
@@ -1,13 +1,58 @@
 import React from 'react';
 import {View, Text, SafeAreaView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Provider as PaperProvider} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import OttContent from './assets/screens/OttContent';
+
+const Tab = createBottomTabNavigator();
+
+function HomeScreen() {
+  return (
+    <Tab.Navigator
+      initialRouteName="OttContent"
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+          switch (route.name) {
+            case 'OttContent': {
+              iconName = focused ? 'movie-open' : 'movie-open-outline';
+              break;
+            }
+            default: {
+              iconName = focused ? 'beaker' : 'beaker';
+              break;
+            }
+          }
+
+          // You can return any component that you like here!
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+        activeTintColor: 'green',
+        inactiveTintColor: 'gray',
+      })}>
+      <Tab.Screen name="OttContent" component={OttContent} />
+    </Tab.Navigator>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
 const App = () => {
   return (
     <PaperProvider>
-      <SafeAreaView style={{flex: 1}}>
-        <OttContent />
-      </SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Homescreen">
+          <Stack.Screen
+            name="Homescreen"
+            component={HomeScreen}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </PaperProvider>
   );
 };
